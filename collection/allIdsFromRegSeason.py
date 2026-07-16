f630c51f63a86971c95c1e3cce4fc7308b489c9d
SlashSaveSmash <95983305+SlashSaveSmash@users.noreply.github.com>
2026-07-16 01:10:58 -0400

    Update allIdsFromRegSeason.py

--- collection/allIdsFromRegSeason.py
+++ collection/allIdsFromRegSeason.py
@@ -1,3 +1,8 @@
+# this needs to call the api (probably using requests)
+# in order to a) accommodate the new 84-game schedule
+# and strike-shortened schedules etc and b) to get
+# only the valid playoff ids
+
 def allIdsFromRegSeason(season:int):
     ids = []
     for i in range(1, 1313):
