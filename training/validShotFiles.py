afe335d60f8e0b7f81b8712a52e750ea136deaa8
SlashSaveSmash <95983305+SlashSaveSmash@users.noreply.github.com>
2026-07-16 15:13:41 -0400

    Made correct file paths
    Using pathlib I made the relative file paths actually work

--- training/validShotFiles.py
+++ training/validShotFiles.py
@@ -1,9 +1,11 @@
+from pathlib import Path
+
 validShotFiles = [
-    'shots2019_2020.txt', 
-    'shots2020_2021.txt', 
-    'shots2021_2022.txt', 
-    'shots2022_2023.txt', 
-    'shots2023_2024.txt', 
-    'shots2024_2025.txt', 
-    'shots2025_2026.txt'
+    Path(__file__).parent.parent/'data'/'shots2019_2020.txt', 
+    Path(__file__).parent.parent/'data'/'shots2020_2021.txt', 
+    Path(__file__).parent.parent/'data'/'shots2021_2022.txt', 
+    Path(__file__).parent.parent/'data'/'shots2022_2023.txt', 
+    Path(__file__).parent.parent/'data'/'shots2023_2024.txt', 
+    Path(__file__).parent.parent/'data'/'shots2024_2025.txt', 
+    Path(__file__).parent.parent/'data'/'shots2025_2026.txt'
 ]
