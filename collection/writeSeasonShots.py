ec337b67190cb8f8b7d06fc4de87adc46b4921c2
SlashSaveSmash <95983305+SlashSaveSmash@users.noreply.github.com>
2026-07-16 17:44:18 -0400

    Made file path correct

--- collection/writeSeasonShots.py
+++ collection/writeSeasonShots.py
@@ -1,11 +1,12 @@
 from allIdsFromRegSeason import allIdsFromRegSeason
 from prepareShotsFromGameId import prepareShotsFromGameId
+from pathlib import Path
 
 def writeSeasonShots(season:int):
     ids = allIdsFromRegSeason(season)
     for id in ids:
         shots = prepareShotsFromGameId(id)
-        with open(f'shots{season}_{season+1}.txt', 'at') as file:
+        with open(Path(__file__).parent.parent/'data'/f'shots{season}_{season+1}.txt', 'at') as file:
             for shot in shots:
                 file.write(str(shot) + '\n')
         file.close()
