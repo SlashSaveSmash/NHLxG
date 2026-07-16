47a34de0f9e4ccc9405d2fef077196e280274c89
SlashSaveSmash <95983305+SlashSaveSmash@users.noreply.github.com>
2026-07-16 18:07:29 -0400

    Update main.py
    Made each of the eight main file functions import statements on main

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from stats.file.teamxGFromFile import teamxGFromFile
+from stats.file.teamrGFromFile import teamrGFromFile
+from stats.file.teamxGAFromFile import teamxGAFromFile
+from stats.file.teamrGAFromFile import teamrGAFromFile
+from stats.file.skaterxGFromFile import skaterxGFromFile
+from stats.file.skaterrGFromFile import skaterrGFromFile
+from stats.file.goaliexGAFromFile import goaliexGAFromFile
+from stats.file.goalierGAFromFile import goalierGAFromFile
 
-print(teamxGFromFile(15, 2025))
+teamxGFromFile(1, 2025) # the xG of the New Jersey Devils from the 2025-2026 season
+teamrGFromFile(15, 2023) # the goals for of the Washington Capitals from the 2023-2024 season
+teamxGAFromFile(68, 2025) # the xGA of the Utah Mammoth from the 2025-2026 season
