120c9da2ebea25612e6db73b0e42d0795a5a876a
SlashSaveSmash <95983305+SlashSaveSmash@users.noreply.github.com>
2026-07-17 08:29:33 -0400

    Began using requests instead of nhlpy

--- stats/api/skaterxGFromAPI.py
+++ stats/api/skaterxGFromAPI.py
@@ -1,4 +1,20 @@
 from client.client import client
+from requests import get
+from collection.prepareShotsFromGameId import prepareShotsFromGameId
+from custom_attributes.xGFromShot import xGFromShot
 
 def skaterxGFromAPI(skaterId, season):
-    raise ValueError
+    xg = 0
+    gameLog = get(f'https://api-web.nhle.com/v1/player/{skaterId}/game-log/{season}{season+1}/2').json()
+    gameIds = [i['gameId'] for i in gameLog['gameLog']]
+    for id in gameIds:
+        shots = prepareShotsFromGameId(id)
+        for shot in shots:
+            if shot['periodDescriptor']['periodType'] in ['REG', 'OT']:
+                try:
+                    if shot['details']['shootingPlayerId'] == skaterId:
+                        xg += xGFromShot(shot)
+                except KeyError:
+                    if shot['details']['scoringPlayerId'] == skaterId:
+                        xg += xGFromShot(shot)
+    return xg
