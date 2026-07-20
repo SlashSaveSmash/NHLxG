1a97b0a81f99464cf64db6c0b71ad4ca4d840ed4
SlashSaveSmash <95983305+SlashSaveSmash@users.noreply.github.com>
2026-07-19 20:43:25 -0400

    Update teamrGFromAPI.py
    Made the call more efficient

--- stats/api/teamrGFromAPI.py
+++ stats/api/teamrGFromAPI.py
@@ -2,8 +2,8 @@ from requests import get
 from collection.teamInfoFromId import teamInfoFromId
 
 def teamrGFromAPI(teamId, season):
-    rg = 0
-    skaters = get(f'https://api-web.nhle.com/v1/club-stats/{teamInfoFromId(teamId)['abbreviation']}/{season}{season+1}/2').json()['skaters']
-    for skater in skaters:
-        rg += skater['goals']
-    return rg
+    teamStats = get(f'https://api.nhle.com/stats/rest/en/team/summary?cayenneExp=seasonId={season}{season+1} and gameTypeId=2').json()['data']
+    for team in teamStats:
+        if team['teamId'] == teamId:
+            return team['goalsFor']
+    return -1
