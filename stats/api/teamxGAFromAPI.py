869b30b488725b5480c85875c4618feaae12d94b
SlashSaveSmash <95983305+SlashSaveSmash@users.noreply.github.com>
2026-07-19 20:47:29 -0400

    Create teamxGAFromAPI.py

---
+++ stats/api/teamxGAFromAPI.py
@@ -0,0 +1,17 @@
+from client import client
+from collection.teamInfoFromId import teamInfoFromId
+from collection.prepareShotsFromGameId import prepareShotsFromGameId
+from custom_attributes.xGFromShot import xGFromShot
+
+def teamxGAFromAPI(teamId, season):
+    xga = 0
+    ids = []
+    for id in client.schedule.team_season_schedule(teamInfoFromId(teamId)['abbreviation'], f'{season}{season+1}')['games']:
+        if f'{season}02' in str(id['id']) or f'{season}03' in str(id['id']):
+            ids.append(id['id'])
+    for id in ids:
+        shots = prepareShotsFromGameId(id)
+        for shot in shots:
+            if not shot['unusable'] and shot['details']['eventOwnerTeamId'] != teamId and shot['periodDescriptor']['periodType'] in ['REG', 'OT']:
+                xga += xGFromShot(shot)
+    return xga
