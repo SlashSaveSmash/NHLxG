e61d35f737027093d99b8fdeed37efe930168c58
SlashSaveSmash <95983305+SlashSaveSmash@users.noreply.github.com>
2026-07-20 07:28:32 -0400

    Create gameStatsFromAPI.py

---
+++ stats/api/gameStatsFromAPI.py
@@ -0,0 +1,47 @@
+from collection.prepareShotsFromGameId import prepareShotsFromGameId
+from custom_attributes.xGFromShot import xGFromShot
+
+def gameStatsFromAPI(gameId):
+    homerG = 0
+    homexG = 0
+    homeSOG = 0
+    homeSO = 0
+
+    awayrG = 0
+    awayxG = 0
+    awaySOG = 0
+    awaySO = 0
+
+    shots = prepareShotsFromGameId(gameId)
+    for shot in shots:
+        homeId = shot['homeTeamId']
+        awayId = shot['awayTeamId']
+        if shot['periodDescriptor']['periodType'] in ['REG', 'OT']:
+            if shot['details']['eventOwnerTeamId'] == shot['homeTeamId']:
+                homeSOG += 1
+                homexG += xGFromShot(shot)
+                if shot['typeDescKey'] == 'goal':
+                    homerG += 1
+            if shot['details']['eventOwnerTeamId'] == shot['awayTeamId']:
+                awaySOG += 1
+                awayxG += xGFromShot(shot)
+                if shot['typeDescKey'] == 'goal':
+                    awayrG += 1
+        if shot['periodDescriptor']['periodType'] == 'SO':
+            if shot['details']['eventOwnerTeamId'] == shot['homeTeamId'] and shot['typeDescKey'] == 'goal':
+                homeSO += 1
+            if shot['details']['eventOwnerTeamId'] == shot['awayTeamId'] and shot['typeDescKey'] == 'goal':
+                awaySO += 1
+    
+    return {
+        'homeId': homeId,
+        'homexG': float(homexG),
+        'homerG': homerG,
+        'homeSOG': homeSOG,
+        'homeSO': homeSO,
+        'awayId': awayId,
+        'awayxG': float(awayxG),
+        'awayrG': awayrG,
+        'awaySOG': awaySOG,
+        'awaySO': awaySO
+    }
