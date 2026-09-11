d5f4a694690c77bc297e924e96caf934404639d4
SlashSaveSmash <95983305+SlashSaveSmash@users.noreply.github.com>
2026-09-11 17:31:08 -0400

    Delete custom_attributes directory

--- custom_attributes/secondsLeftInPeriod.py
+++
@@ -1,6 +0,0 @@
-def secondsLeftInPeriod(shot:dict):
-    seconds = 0
-    timeRemaining = shot['timeRemaining']
-    seconds += int(timeRemaining[0:2])*60
-    seconds += int(timeRemaining[3:5])
-    return seconds
