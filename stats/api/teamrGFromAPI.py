508e74664ed98406ed805da9e6d9b84ce5164d8d
SlashSaveSmash <95983305+SlashSaveSmash@users.noreply.github.com>
2026-09-11 17:31:47 -0400

    Delete stats directory

--- stats/api/teamrGFromAPI.py
+++
@@ -1,9 +0,0 @@
-from requests import get
-from collection.teamInfoFromId import teamInfoFromId
-
-def teamrGFromAPI(teamId, season):
-    teamStats = get(f'https://api.nhle.com/stats/rest/en/team/summary?cayenneExp=seasonId={season}{season+1} and gameTypeId=2').json()['data']
-    for team in teamStats:
-        if team['teamId'] == teamId:
-            return team['goalsFor']
-    return -1
